75e5fa4d27624867a35035c7c6ce8ab90479d624
guustavocruuzs <guustavocruuzsilva@gmail.com>
2022-10-18 14:45:18 -0300

    Adicionando comentarios no teste

--- cypress/e2e/fluxoOndeUsarCartao.cy.js
+++ cypress/e2e/fluxoOndeUsarCartao.cy.js
@@ -1,19 +1,23 @@
 /// <reference types="Cypress" />
+//Cenário de teste 
+describe('Fluxo de visualização do mapa - onde aceita VR', () => {
 
-describe('Testes automatizados VR', () => {
-    
-  it('Acessando pagina incial da VR"', () => {
-      cy.visit('https://www.vr.com.br/')
-      cy.title().should('be.equal', 'VR Benefícios - Vale Refeição, Vale Alimentação e Transporte')
-  })
-  it('Acessando pagina "Pra Você"', () => {
-      cy.get(':nth-child(3) > .vr-quick-navigation__link').click()
-      cy.title().should('be.equal', 'VR Benefícios - Onde aceita VR')
-      cy.url().should('eq', 'https://www.vr.com.br/onde-aceita.htm')
-  })
+    //ACessando pagina inicial para poder navegar até a pagina de onde aceita VR
+    it('Acessando pagina incial da VR"', () => {
+        cy.visit('https://www.vr.com.br/')
+        cy.title().should('be.equal', 'VR Benefícios - Vale Refeição, Vale Alimentação e Transporte')
+    })
 
-  it('Visualizando mapa', () => {
-      cy.get('.vr-hero__actions > .vr-button--negative').click()
-      cy.get('#rede-credenciada.vr-modal.vr-modal--map').should('be.visible')
-  })
+    //Acessando a pagina pra você para poder acesasr o mapa de visualização
+    it('Acessando pagina "Pra Você"', () => {
+        cy.get(':nth-child(3) > .vr-quick-navigation__link').click()
+        cy.title().should('be.equal', 'VR Benefícios - Onde aceita VR')
+        cy.url().should('eq', 'https://www.vr.com.br/onde-aceita.htm')
+    })
+
+    //Garantindo que o mapa ta sendo exibido atraves da modal de mapa
+    it('Visualizando mapa', () => {
+        cy.get('.vr-hero__actions > .vr-button--negative').click()
+        cy.get('#rede-credenciada.vr-modal.vr-modal--map').should('be.visible')
+    })
 })
